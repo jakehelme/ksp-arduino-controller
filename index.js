f596f571cbb656552f73ea78fb4944684f6066e6
Jake Helme <jake.helme@gmail.com>
2017-03-08 00:52:59 +0200

    Adding second joystick. Having problems with throttle control for now

--- index.js
+++ index.js
@@ -9,17 +9,19 @@ let client, button, led;
 let game = {};
 
 const init = () => {
-  connectBoard();
   connectClient();
+
 }
 
 const connectClient = () => {
   client = Client();
+
   client.rpc.on('open', () => {
     console.log('Connected to KSP');
     client.rpc.on('message', getActiveVesselComplete);
     client.rpc.send(client.services.spaceCenter.getActiveVessel());
   });
+
   client.rpc.on('error', function (err) {
     console.log(util.format('Error : %j', err));
     process.exit(1);
@@ -38,17 +40,37 @@ const connectBoard = () => {
       isPullup: true
     });
 
-    var joystick = new five.Joystick({
+    let joystick1 = new five.Joystick({
       pins: ['A0', 'A1']
+    });
+
+    let joystick2 = new five.Joystick({
+      pins: ['A2', 'A3']
     });
 
     button.on('down', function (value) {
       client.rpc.send(client.services.spaceCenter.controlActivateNextStage(game.vessel.control.id));
     });
 
-    joystick.on('change', function () {
-      client.rpc.send(client.services.spaceCenter.controlSetPitch(game.vessel.control.id, this.x));
-      client.rpc.send(client.services.spaceCenter.controlSetRoll(game.vessel.control.id, this.y));
+    joystick1.on('change', function () {
+      const step = 0.01;
+      client.rpc.send(client.services.spaceCenter.controlSetYaw(game.vessel.control.id, this.x));
+      if (this.y > 0.5 && game.vessel.control.throttle < 1.0) {
+        replaceMessageHandler(getThrottle);
+        client.rpc.send(client.services.spaceCenter.controlSetThrottle(game.vessel.control.id, game.vessel.control.throttle + step));
+        console.log(game.vessel.control.throttle);
+      } else if (this.y < -0.5 && game.vessel.control.throttle > 0.0) {
+        replaceMessageHandler(getThrottle);
+        client.rpc.send(client.services.spaceCenter.controlSetThrottle(game.vessel.control.id, game.vessel.control.throttle - step));
+        console.log(game.vessel.control.throttle);
+      }
+      // console.log(`x: ${this.x} y: ${this.y}`);
+      // console.log(`y: ${this.y}`);
+    });
+
+    joystick2.on('change', function () {
+      client.rpc.send(client.services.spaceCenter.controlSetRoll(game.vessel.control.id, this.x));
+      client.rpc.send(client.services.spaceCenter.controlSetPitch(game.vessel.control.id, this.y));
     });
 
     console.log('Board ready');
@@ -67,11 +89,19 @@ const getActiveVesselControlComplete = (response) => {
   game.vessel.control = {
     id: getFirstResult(response)
   };
-  replaceMessageHandler(() => {});
+  replaceMessageHandler(getThrottleComplete);
+  client.rpc.send(client.services.spaceCenter.controlGetThrottle(game.vessel.control.id));
+  connectBoard();
 }
 
-const setThrottleToFullComplete = (response) => {
-  replaceMessageHandler(launched);
+const getThrottle = () => {
+  replaceMessageHandler(getThrottleComplete);
+  client.rpc.send(client.services.spaceCenter.controlGetThrottle(game.vessel.control.id));
+}
+
+const getThrottleComplete = (response) => {
+  game.vessel.control.throttle = getFirstResult(response);
+  replaceMessageHandler(() => {});
 }
 
 const getFirstResult = (response) => {
