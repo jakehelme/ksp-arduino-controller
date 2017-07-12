dfe8e95fdbdaa039b9b73f09986d9970df743b42
Jake Helme <jake.helme@gmail.com>
2017-07-13 00:23:08 +0200

    It lives in a semi proper casing now. And has more buttons! Added Action Group buttons 1-9, fixed throttle control, added abort switch, fixed toggles.

--- src/index.js
+++ src/index.js
@@ -20,7 +20,9 @@ let state = {
         id: null,
         controlId: null,
         surfaceReference: null,
-        surfaceFlightId: null
+        surfaceVelocityReference: null,
+        surfaceFlightId: null,
+        surfaceVelocityFlightId: null
     }
 };
 
@@ -43,7 +45,7 @@ const clientCreated = (err, clientCreated) => {
             connectToStreamServer,
             getVesselInfo,
             getMoreVesselInfo,
-            addPitchToStream,
+            addSpeedToStream,
             connectBoard
         ],
         function (err) {
@@ -76,7 +78,7 @@ const getResultN = (response, n) => {
 const streamUpdate = (streamState) => {
     if (moment.utc().isAfter(nextLogTimer)) {
         if (typeof lcd !== 'undefined') {
-            lcd.clear().print(`Pitch: ${Math.round(streamState.pitch * 100) / 100}`);
+            lcd.clear().print(`Speed: ${streamState.speed}`);
         }
         incrementNextLogTimer();
     }
@@ -92,21 +94,34 @@ const connectBoard = (callback) => {
         debug: false,
     });
     board.on('ready', () => {
+
         console.log('board connected');
+
+        let abort = new five.Switch(40);
+        let toggle1 = new five.Switch(44);
+        let toggle2 = new five.Switch(43);
+        let toggle3 = new five.Switch(42);
+        let toggle4 = new five.Switch(41);
+        let action1 = new five.Button(53);
+        let action2 = new five.Button(52);
+        let action3 = new five.Button(51);
+        let action4 = new five.Button(50);
+        let action5 = new five.Button(49);
+        let action6 = new five.Button(48);
+        let action7 = new five.Button(47);
+        let action8 = new five.Button(46);
+        let action9 = new five.Button(45);
+        let slider = new five.Sensor("A6");
+
         let joystick1Button = new five.Button({
-            pin: 50,
+            pin: 39,
             isPullup: true
         });
 
         let joystick2Button = new five.Button({
-            pin: 48,
+            pin: 38,
             isPullup: true
         });
-
-        let toggle1 = new five.Switch(51);
-        let toggle2 = new five.Switch(49);
-        // let toggle3 = new five.Switch();
-        let toggle4 = new five.Switch(47);
 
         let joystick1 = new five.Joystick({
             pins: ['A0', 'A1'],
@@ -116,8 +131,6 @@ const connectBoard = (callback) => {
         let joystick2 = new five.Joystick({
             pins: ['A2', 'A3']
         });
-
-        let slider = new five.Sensor("A4");
 
         lcd = new five.LCD({
             pins: [7, 8, 9, 10, 11, 12],
@@ -134,19 +147,27 @@ const connectBoard = (callback) => {
         });
 
         toggle1.on("close", function () {
-            client.send(client.services.spaceCenter.controlSetSas(state.vessel.controlId, true));
+            client.send(client.services.spaceCenter.controlSetLights(state.vessel.controlId, true));
         });
 
         toggle1.on("open", function () {
-            client.send(client.services.spaceCenter.controlSetSas(state.vessel.controlId, false));
+            client.send(client.services.spaceCenter.controlSetLights(state.vessel.controlId, false));
         });
 
         toggle2.on("close", function () {
-            client.send(client.services.spaceCenter.controlSetSas(state.vessel.controlId, true));
+            client.send(client.services.spaceCenter.controlSetBrakes(state.vessel.controlId, true));
         });
 
         toggle2.on("open", function () {
-            client.send(client.services.spaceCenter.controlSetSas(state.vessel.controlId, false));
+            client.send(client.services.spaceCenter.controlSetBrakes(state.vessel.controlId, false));
+        });
+
+        toggle3.on("close", function () {
+            client.send(client.services.spaceCenter.controlSetRcs(state.vessel.controlId, true));
+        });
+
+        toggle3.on("open", function () {
+            client.send(client.services.spaceCenter.controlSetRcs(state.vessel.controlId, false));
         });
 
         toggle4.on("close", function () {
@@ -157,9 +178,53 @@ const connectBoard = (callback) => {
             client.send(client.services.spaceCenter.controlSetSas(state.vessel.controlId, false));
         });
 
+        abort.on("open", function () {
+            client.send(client.services.spaceCenter.controlSetAbort(state.vessel.controlId, false));
+        });
+
+        abort.on("close", function () {
+            client.send(client.services.spaceCenter.controlSetAbort(state.vessel.controlId, true));
+        });
+
+        action1.on("press", function () {
+            client.send(client.services.spaceCenter.controlToggleActionGroup(state.vessel.controlId, 1));
+        });
+
+        action2.on("press", function () {
+            client.send(client.services.spaceCenter.controlToggleActionGroup(state.vessel.controlId, 2));
+        });
+
+        action3.on("press", function () {
+            client.send(client.services.spaceCenter.controlToggleActionGroup(state.vessel.controlId, 3));
+        });
+
+        action4.on("press", function () {
+            client.send(client.services.spaceCenter.controlToggleActionGroup(state.vessel.controlId, 4));
+        });
+
+        action5.on("press", function () {
+            client.send(client.services.spaceCenter.controlToggleActionGroup(state.vessel.controlId, 5));
+        });
+
+        action6.on("press", function () {
+            client.send(client.services.spaceCenter.controlToggleActionGroup(state.vessel.controlId, 6));
+        });
+
+        action7.on("press", function () {
+            client.send(client.services.spaceCenter.controlToggleActionGroup(state.vessel.controlId, 7));
+        });
+
+        action8.on("press", function () {
+            client.send(client.services.spaceCenter.controlToggleActionGroup(state.vessel.controlId, 8));
+        });
+
+        action9.on("press", function () {
+            client.send(client.services.spaceCenter.controlToggleActionGroup(state.vessel.controlId, 9));
+        });
+
         joystick1.on('change', function () {
             client.send(client.services.spaceCenter.controlSetYaw(state.vessel.controlId, getJoystickValue(this.x)));
-            client.send(client.services.spaceCenter.controlSetThrottle(state.vessel.controlId, changeThrottle(this.y)));
+            // client.send(client.services.spaceCenter.controlSetThrottle(state.vessel.controlId, changeThrottle(this.y)));
         });
 
         joystick2.on('change', function () {
@@ -167,9 +232,9 @@ const connectBoard = (callback) => {
             client.send(client.services.spaceCenter.controlSetPitch(state.vessel.controlId, getJoystickValue(this.y)));
         });
 
-        // slider.scale([0, 100]).on("change", function () {
-        //     console.log("slide", this.value);
-        // });
+        slider.scale([0, 1]).on("change", function () {
+            client.send(client.services.spaceCenter.controlSetThrottle(state.vessel.controlId, 1 - this.value));
+        });
 
         console.log('Board ready');
         return callback();
@@ -186,20 +251,20 @@ const getJoystickValue = (input) => {
     }
 }
 
-const changeThrottle = (input) => {
-    if (input > joystickDeadzone) {
-        state.vessel.throttle += (step + (input / inputScale));
-    } else if (input < -joystickDeadzone) {
-        state.vessel.throttle -= (step - (input / inputScale));
-    }
+// const changeThrottle = (input) => {
+//     if (input > joystickDeadzone) {
+//         state.vessel.throttle += (step + (input / inputScale));
+//     } else if (input < -joystickDeadzone) {
+//         state.vessel.throttle -= (step - (input / inputScale));
+//     }
 
-    if (state.vessel.throttle > 1) {
-        state.vessel.throttle = 1;
-    } else if (state.vessel.throttle < 0) {
-        state.vessel.throttle = 0;
-    }
-    return state.vessel.throttle;
-}
+//     if (state.vessel.throttle > 1) {
+//         state.vessel.throttle = 1;
+//     } else if (state.vessel.throttle < 0) {
+//         state.vessel.throttle = 0;
+//     }
+//     return state.vessel.throttle;
+// }
 
 const getInitialInfo = (callback) => {
     let calls = [
@@ -228,7 +293,8 @@ const connectToStreamServer = (callback) => {
 const getVesselInfo = (callback) => {
     let calls = [
         client.services.spaceCenter.vesselGetControl(state.vessel.id),
-        client.services.spaceCenter.vesselGetSurfaceReferenceFrame(state.vessel.id)
+        client.services.spaceCenter.vesselGetSurfaceReferenceFrame(state.vessel.id),
+        client.services.spaceCenter.vesselGetSurfaceVelocityReferenceFrame(state.vessel.id)
     ];
 
     client.send(calls, function (err, response) {
@@ -237,6 +303,7 @@ const getVesselInfo = (callback) => {
         }
         state.vessel.controlId = getFirstResult(response);
         state.vessel.surfaceReference = getResultN(response, 1);
+        state.vessel.surfaceVelocityReference = getResultN(response, 2);
         return callback();
     });
 }
@@ -244,7 +311,8 @@ const getVesselInfo = (callback) => {
 const getMoreVesselInfo = (callback) => {
     let calls = [
         client.services.spaceCenter.controlGetThrottle(state.vessel.controlId),
-        client.services.spaceCenter.vesselFlight(state.vessel.id, state.vessel.surfaceReference)
+        client.services.spaceCenter.vesselFlight(state.vessel.id, state.vessel.surfaceReference),
+        client.services.spaceCenter.vesselFlight(state.vessel.id, state.vessel.surfaceVelocityReference)
     ];
 
     client.send(calls, (err, response) => {
@@ -253,11 +321,12 @@ const getMoreVesselInfo = (callback) => {
         }
         state.vessel.throttle = getFirstResult(response);
         state.vessel.surfaceFlightId = getResultN(response, 1);
+        state.vessel.surfaceVelocityFlightId = getResultN(response, 2);
         return callback();
     });
 };
 
-const addPitchToStream = (callback) => {
-    const call = client.services.spaceCenter.flightGetPitch(state.vessel.surfaceFlightId);
-    client.addStream(call, 'pitch', callback);
+const addSpeedToStream = (callback) => {
+    const call = client.services.spaceCenter.flightGetSpeed(state.vessel.surfaceVelocityFlightId);
+    client.addStream(call, 'speed', callback);
 }
