693dfc0efaefb8dd46aec3f0e24a45672e4d9a3d
Jake Helme <jake.helme@gmail.com>
2018-02-07 23:40:24 +0200

    Got basic functionality of leap working. This will be hard to do well :/

---
+++ src/leap.js
@@ -0,0 +1,58 @@
+const leapjs = require('leapjs');
+const controller = new leapjs.Controller({enableGestures: true});
+
+let latestGesture;
+let gestureCount = 0;
+
+function consoleUpdate(){
+	process.stdout.clearLine();
+	process.stdout.cursorTo(0);
+	process.stdout.write(`Gesture Count: ${gestureCount} - Latest gesture: ${latestGesture}`);
+	// process.stdout.write(`Latest gesture: ${latestGesture}\n`);
+}
+
+controller.on('deviceFrame', function(frame) {
+	// loop through available gestures
+	for(var i = 0; i < frame.gestures.length; i++){
+		var gesture = frame.gestures[i];
+		var type    = gesture.type;          
+
+		switch( type ){
+
+		case 'circle':
+			if (gesture.state == 'stop') {
+				latestGesture = 'circle';
+				gestureCount++;
+				consoleUpdate();
+			}
+			break;
+
+		case 'swipe':
+			if (gesture.state == 'stop') {
+				latestGesture = 'swipe';
+				gestureCount++;
+				consoleUpdate();
+			}
+			break;
+
+		case 'screenTap':
+			if (gesture.state == 'stop') {
+				latestGesture = 'screen tap';
+				gestureCount++;
+				consoleUpdate();
+			}
+			break;
+
+		case 'keyTap':
+			if (gesture.state == 'stop') {
+				latestGesture = 'key tap';
+				gestureCount++;
+				consoleUpdate();
+			}
+			break;
+
+		}
+	}
+});
+
+controller.connect();
