8e0a529c172c983873333c8f2a144c44a26d68e5
Jake Helme <jake.helme@gmail.com>
2018-03-04 23:29:27 +0200

    Silly cleanup

--- test/util/rightPad-test.js
+++ test/util/rightPad-test.js
@@ -1,11 +1,9 @@
 const rightPad = require('./../../src/util/rightPad');
 const assert = require('assert');
 
-
 describe('rightPad', function() {
 	it('should pad things correctly', function() {
 		const actual = rightPad('hello',7,' ');
 		assert.equal(actual, 'hello  ');
 	});
 });
-
